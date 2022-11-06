30fa20d321d93f1289ed2cc4b4d0b8e8bbd0464d
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-11-06 18:26:43 +0700

    remove user name in details

--- TakeFood.UserOrder/ViewModel/Dtos/Order/OrderDetailDto.cs
+++ TakeFood.UserOrder/ViewModel/Dtos/Order/OrderDetailDto.cs
@@ -4,7 +4,6 @@ public class OrderDetailDto
 {
     public string OrderId { get; set; }
     public string Address { get; set; }
-    public string UserName { get; set; }
     public string PhoneNumber { get; set; }
     public string PaymentMethod { get; set; }
     public string State { get; set; }
