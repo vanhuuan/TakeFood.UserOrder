e8a932d6081fb6f69ad04d2f768df41dc27b41e9
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-11-29 11:51:26 +0700

    update middleware

--- TakeFood.UserOrder/Middleware/AuthenticationMiddleware.cs
+++ TakeFood.UserOrder/Middleware/AuthenticationMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
 using StoreService.Service;
 using System.IdentityModel.Tokens.Jwt;
 
@@ -8,53 +9,21 @@ public class AuthenticationMiddleware
 {
     private readonly RequestDelegate _next;
 
-    private IUserService userService;
-    private IJwtService jwtService;
-
     public AuthenticationMiddleware(RequestDelegate next)
     {
         _next = next;
     }
-    public async Task InvokeAsync(HttpContext context, IUserService _userService, IJwtService _jwtService)
+    public async Task InvokeAsync(HttpContext context)
     {
         try
         {
-            userService = _userService;
-            jwtService = _jwtService;
-            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            if (token != null)
-                attachUserToContext(context, token, userService, jwtService);
+            var id = context.Request.Headers.TryGetValue("Uid", out StringValues sv);
+            context.Items["Id"] = sv.ToString();
             await _next(context);
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
-        }
-    }
-    private async void attachUserToContext(HttpContext context, string token, IUserService userService, IJwtService jwtService)
-    {
-        try
-        {
-            jwtService.ValidSecurityToken(token);
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-            JwtSecurityToken jwtToken = tokenHandler.ReadToken(token!) as JwtSecurityToken;
-            string id = jwtService.GetId(token);
-            List<string> roles = jwtService.GetRoles(token);
-            context.Items["Role"] = roles;
-            context.Items["Id"] = id;
-            context.Items["TokenExp"] = false;
-        }
-        catch (SecurityTokenExpiredException err)
-        {
-            context.Items["TokenExp"] = true;
-            context.Items["Id"] = "0";
-            context.Items["Role"] = "UnAuth";
-        }
-        catch (Exception err)
-        {
-            context.Items["TokenExp"] = true;
-            context.Items["Id"] = "0";
-            context.Items["Role"] = "UnAuth";
         }
     }
 }
